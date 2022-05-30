3b8b24f055d7fe462fd2ffdf0345e35c78f7fec3
daesung6756 <49222243+daesung6756@users.noreply.github.com>
2022-05-30 10:37:33 +0900

    404 처리 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,12 @@ const Test2 = () => import('../views/About.vue');
 const fileNotFound = () => import('../views/About.vue');
 
 const routes = [
+  { path: '/:pathMatch(.*)*', redirect: '/404' },
   { path: '/', name: 'home', component: Home },
   { path: '/about', name: 'about', component: About },
   { path: '/test1', name: 'test1', component: Test1 },
   { path: '/test2', name: 'test2', component: Test2 },
-  { path: '/:pathMatch(.*)*', redirect: '/404', component: fileNotFound },
+  { path: '/404', name: 'fileNotFound', component: fileNotFound },
 ];
 
 const router = createRouter({
