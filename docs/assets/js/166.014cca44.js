4fe34a70ce1bf48d5b206ae7d10b43f6df205ac8
daesung6756 <49222243+daesung6756@users.noreply.github.com>
2022-05-30 11:07:20 +0900

    404 처리 테스트

---
+++ docs/assets/js/166.014cca44.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_setting"]=self["webpackChunkvue_setting"]||[]).push([[166],{9166:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var u=t(3396);function r(n,e,t,r,i,s){return(0,u.wg)(),(0,u.iD)("div",null," 404 파일 없습니다. ")}var i={name:"fileNotFound"},s=t(89);const a=(0,s.Z)(i,[["render",r]]);var c=a}}]);
+//# sourceMappingURL=166.014cca44.js.map
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const Home = () => import('../views/Home.vue');
 const About = () => import('../views/About.vue');
 const Test1 = () => import('../views/About.vue');
 const Test2 = () => import('../views/About.vue');
-const fileNotFound = () => import('../views/About.vue');
+const fileNotFound = () => import('../views/fileNotFound.vue');
 
 const routes = [
   { path: '/:pathMatch(.*)*', redirect: '/fileNotFound' },
